9a24135cb99e148559d914766f265873c9becbab
shuvro baset <md.abdul.baset75@gmail.com>
2021-09-04 23:47:20 +0600

    my-app

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          My first React app. It's really cool.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          #shuvro75
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
